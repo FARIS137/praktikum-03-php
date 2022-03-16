43d3bd115d8e47f871541707163742a969871b50
FARIS137 <96936492+FARIS137@users.noreply.github.com>
2022-03-16 17:22:08 +0700

    Update header.php

--- startbootstrap-sb-admin-gh-pages/layout/header.php
+++ startbootstrap-sb-admin-gh-pages/layout/header.php
@@ -5,9 +5,33 @@
  </head>
  <body>
 
- <header role="banner">
- <h2>Student Activity Score - STTNF</h2>
- <a href="#">Home</a> | <a href="#">Activity</a> | <a href="#">My Score</a>
- | <a href="#">Login</a>
- <hr/>
- </header>
+ <header id="masthead" class="site-header">
+		<div class="container-fluid">
+			<div class="row no-gutters " style="">
+				<div class="col-lg-2 ">
+					<div class="site-branding">
+					<a href="https://webdev-id.com/">Student Activity Score - STTNF</a>
+										</div><!-- .site-branding -->
+				</div>
+				<div class="col-lg-10 ">
+					<nav class="navbar navbar-expand-lg" role="navigation">
+					 
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<button class="mobile-search"></button> 
+							<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse justify-content-end"><ul id="primary-menu" class="nav navbar-nav"><li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-1163" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1163 nav-item"><a title="Tentang WDI" href="https://webdev-id.com/tentang-wdi" class="nav-link">Home</a></li>
+<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-7" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7 nav-item"><a title="Cari Developer" target="_blank" href="https://webdev-id.com/web-developer" class="nav-link">Activity</a></li>
+<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-8" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8 nav-item"><a title="Cari Proyek" target="_blank" href="https://webdev-id.com/proyek-digital" class="nav-link">My score</a></li>
+<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-9" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9 nav-item"><a title="Lowongan Kerja" target="_blank" href="https://webdev-id.com/karir" class="nav-link">Lowongan Kerja</a></li>
+<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-50" class="menu-daftar menu-item menu-item-type-custom menu-item-object-custom menu-item-50 nav-item"><a title="Daftar" target="_blank" href="https://webdev-id.com/daftar" class="nav-link">Daftar</a></li>
+<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-49" class="menu-login menu-item menu-item-type-custom menu-item-object-custom menu-item-49 nav-item"><a title="Login" target="_blank" href="https://webdev-id.com/login" class="nav-link">Login</a></li>
+</ul></div>					 
+					</nav>
+				</div>
+			</div>
+			
+		</div>
+		
+	</header><
